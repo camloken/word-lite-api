081fbef3df85ff8238c68d1be3f2f90e07642d13
Cameron Loken <camloken@gmail.com>
2023-06-02 21:00:37 -0600

    Remove node_modules

---
+++ README.md
@@ -0,0 +1,19 @@
+# bbl-api
+
+This API returns the the following Bibles in JSON format
+NKJV - New King James Version
+NASB - New American Standard Bible
+ESV - English Standard Version
+LITV - Green's Literal Translation
+GB - Geneva Bible
+
+
+Endpoint 1: https://bbl-api.vercel.app/nkjv (returns the whole NKJV Bible)
+
+Endpoint 2: https://bbl-api.vercel.app/nkjv/genesis (returns the book of genesis)
+
+Endpoint 3: https://bbl-api.vercel.app//nkjv/genesis/3 (returns the book of genesis chapter 3)
+
+Endpoint 4: https://bbl-api.vercel.app//nkjv/genesis/3/1 (returns the book of genesis chapter 3 verse 1)
+
+Note: for '1 John' don't use spaces (e.g. '1John' or '1john')
---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const nkjv = require('./nkjv.json')
+const nasb = require('./nasb.json')
+const esv = require('./esv.json')
+const geneva = require('./geneva.json')
+require("dotenv").config()
+
+const port = process.env.PORT || 8080
+const app = express()
+let bible = nkjv
+
+function getBibleVersion(version) {
+  switch (version) {
+    case 'esv':
+      bible = esv 
+      break
+    case 'litv':
+      bible = litv 
+      break
+    case 'geneva':
+      bible = geneva 
+      break
+    case 'nasb':
+      bible = nasb 
+      break
+    default:
+      bible = nkjv
+  }
+}
+
+app.get('/', (req, res) => {
+  res.send('Please add the bible abbreviation /NKJV, /NASB, /LITV, /ESV, /GENEVA or (/GB)')
+})
+
+app.get('/:version', (req, res) => {
+  res.json(bible)
+})
+
+app.get('/:version/:book', async (req, res) => {
+  getBibleVersion(req.params.version)
+  const bookName = req.params.book.toLowerCase().replace(' ', '')
+  console.log(`Retrieving ${req.params.version.toUpperCase()} ${req.params.book}`)
+  const json = await bible.books.filter(book => book.name.toLowerCase().replace(' ', '') === bookName)
+  res.json(json[0])
+ })
+ 
+app.get('/:version/:book/:chapter', async (req, res) => {
+  getBibleVersion(req.params.version)
+  const bookName = req.params.book.toLowerCase().replace(' ', '')
+  const chapter = Number(req.params.chapter)
+  console.log(`Retrieving ${req.params.version.toUpperCase()} ${req.params.book} ${chapter}`)
+  const json = await bible.books.filter(book => book.name.toLowerCase().replace(' ', '') === bookName)
+  res.json(json[0].chapters[chapter - 1])
+})
+
+app.get('/:version/:book/:chapter/:verse', async (req, res) => {
+  getBibleVersion(req.params.version)
+  const bookName = req.params.book.toLowerCase().replace(' ', '')
+  const chapter = Number(req.params.chapter)
+  const verse = Number(req.params.verse)
+  console.log(`Retrieving ${req.params.version.toUpperCase()} ${req.params.book} ${chapter}:${verse}`)
+  const json = await bible.books.filter(book => book.name.toLowerCase().replace(' ', '') === bookName)
+  res.json(json[0].chapters[chapter - 1].verses[verse - 1])
+})
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port :${port}`)
+})
+
+module.exports = app
+
+
